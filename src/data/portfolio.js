2f0e56aafce302403ec324f21b6452fec91035ca
Jonathan <jonathanroos60@gmail.com>
2022-12-27 12:23:19 +1000

    initial commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Project Flying Fox',
+        imgUrl: '',
+        stack: ['HTML', 'CSS'],
+        link: '',
+    },
+    {
+        title: 'Project 2',
+        imgUrl: '',
+        stack: ['HTML', 'CSS'],
+        link: '',
+    },
+    {
+        title: 'Project 3',
+        imgUrl: '',
+        stack: ['HTML', 'CSS'],
+        link: '',
+    },
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        year: 'Current',
+        title: 'Student',
+        duration: '2019 - 2023',
+        details: 'I am currently in my final year of studying for my Bachelor of Business and Bachelor of Information Technology majoring in Software Development.',
+    },
+    {
+        year: 'Current',
+        title: 'Intern - ECM Administrator',
+        duration: '3 months',
+        details: 'Joined the Information Services team at APA located at the Brisbane office. ',
+    },
+    {
+        year: 'May - Nov 2022',
+        title: 'Trainee Helpdesk Administrator',
+        duration: '7 months',
+        details: 'Was a part of the team at Evolve Technologies',
+    },
+    {
+        year: '2020 - 2022',
+        title: 'Barista',
+        duration: '2 Years',
+        details: 'Held multiple hospitality roles as a barista at Jamaica Blue and Pages Cafe.',
+    },
+]
